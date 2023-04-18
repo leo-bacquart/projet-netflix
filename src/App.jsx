b25f62c5bf6495ce5a7043bda94e955d67ab9b61
Léo <bacquartl@icloud.com>
2023-04-18 16:43:23 +0200

    GET

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,20 @@ import * as React from 'react';
 import './App.css'
 
 function App() {
-    const [trending, setTrending] = React.useState([])
+    const [trending, setTrending] = React.useState();
+    React.useEffect(() => {
+        fetch('https://api.themoviedb.org/3/movie/popular?api_key=5c0913b3a2bf3d6729475eaa432c0034&page=1')
+            .then(response => response.json())
+            .then(json => setTrending(json))
+            .catch(error => console.log(error))
+
+        console.log(trending)
+    })
+
 
     return (
         <div>
-            <Search donnee1={'3'}></Search>
+            <h1>Netflix</h1>
         </div>
     )
 }
