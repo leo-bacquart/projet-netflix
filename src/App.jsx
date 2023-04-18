43f4fe97cad61a3a299b21a0e1acd52bd54527bc
Léo <bacquartl@icloud.com>
2023-04-18 16:36:11 +0200

    Commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import * as React from 'react';
 import './App.css'
 
 function App() {
+    const [trending, setTrending] = React.useState([])
 
-  return (
-    <div className="App">
-        <h1>Netflix</h1>
-    </div>
-  )
+    return (
+        <div>
+            <Search donnee1={'3'}></Search>
+        </div>
+    )
 }
+
+
 
 export default App
