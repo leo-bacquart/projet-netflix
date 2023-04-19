6cff88a9389327e5510cebf97f3fee69529196c2
EmmaCoco6 <emma.coco.pro@gmail.com>
2023-04-19 14:44:52 +0200

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,34 @@
 import * as React from "react";
+import { Outlet, Link } from "react-router-dom";
+import '../style/Header.scss'
 
 const Header = () => {
 
     return (
-
-    )
-}
+        <>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Accueil</Link>
+              </li>
+              <li>
+                <Link to="/blogs">Séries</Link>
+              </li>
+              <li>
+                <Link to="/contact">Films</Link>
+              </li>
+              <li>
+                <Link to="/contact">Nouveautés les plus regardées</Link>
+              </li>
+              <li>
+                <Link to="/contact">Ma liste</Link>
+              </li>
+            </ul>
+          </nav>
+    
+          <Outlet />
+        </>
+      )
+    };
 
 export default Header
