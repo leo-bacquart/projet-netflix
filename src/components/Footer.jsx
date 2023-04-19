61e6c0261e64cb46784e7b8a28cb393a8c927ae5
Léo <bacquartl@icloud.com>
2023-04-19 14:44:58 +0200

    Base Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,16 @@ import * as React from "react";
 const Footer = () => {
 
     return (
-
+        <>
+            <nav>
+                <ul>
+                    <li>A</li>
+                    <li>B</li>
+                    <li>C</li>
+                    <li>D</li>
+                </ul>
+            </nav>
+        </>
     )
 }
 
