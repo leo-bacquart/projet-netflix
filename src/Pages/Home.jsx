6d65919fba790e0e56427d20bed89aac4bc2a341
EmmaCoco6 <emma.coco.pro@gmail.com>
2023-04-19 13:15:23 +0200

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -8,6 +8,9 @@ const Home = () => {
         <div>
             <List params={'/trending/movie/day'}>Tendance</List>
             <List params={'/movie/now_playing'}>En ce moment</List>
+            <List params={'/movie/popular'}>Populaires</List>
+            <List params={'/movie/top_rated'}>Mieux notés</List>
+            <List params={'/movie/upcoming'}>À venir</List>
         </div>
     )
 
