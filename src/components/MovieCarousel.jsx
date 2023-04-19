cd772f0f9cb78c8c406659b952d45d101fcb97e1
Léo <bacquartl@icloud.com>
2023-04-19 11:32:30 +0200

    Comment MovieCarousel

--- src/components/MovieCarousel.jsx
+++ src/components/MovieCarousel.jsx
@@ -1,11 +1,13 @@
 import * as React from "react";
 import * as API from "./callAPI.jsx";
+import '../style/Carousel.scss'
 
-const List = ({params, children}) => {
-    const [movieList, setMovieList] = React.useState([])
+const List = ({params, children}) => { /* params = paramètres de recherche dans l'API, children = Prend en paramètre le contenu de la balise */
+    const [movieList, setMovieList] = React.useState([]) /* Création state MovieList, définition par un tableau vide */
 
     React.useEffect(() => {
-        fetch(`${API.ID}${params}?api_key=${API.KEY}`)
+        /* Call GET API  */
+        fetch(`${API.ID}${params}?api_key=${API.KEY}`) /* Lien d'appel de l'api */
             .then((response) => response.json())
             .then((data) => {
                 setMovieList(data.results);
@@ -13,13 +15,13 @@ const List = ({params, children}) => {
             .catch((error) => {
                 console.log(error.message);
             });
-    }, [])
+    }, [] /* Pas de dépendance, la fonction s'éxécute une seule fois */)
 
     return (
         <div>
             <h2>{children}</h2>
             <ul>
-                {movieList.map((movie) => <Item key={movie.key} movie={movie}/> )}
+                {movieList.map((movie) => <Item key={movie.key} movie={movie}/> )} {/* Création d'une instance movie par partie du tableau */}
             </ul>
         </div>
     )
@@ -28,7 +30,7 @@ const List = ({params, children}) => {
 
 const Item = ({movie}) => (
     <li>
-        <img src={"https://image.tmdb.org/t/p/w500/" + movie.poster_path} alt={movie.key + "poster"}/>
+        <img src={"https://image.tmdb.org/t/p/w500/" + movie.poster_path} alt={movie.key + "poster"}/> {/*Récupération de l'affiche*/}
     </li>
 )
 
