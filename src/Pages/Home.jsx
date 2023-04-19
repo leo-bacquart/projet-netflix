4c65d579d048d3b31d8e3fb7fa83e722e5eb3155
EmmaCoco6 <emma.coco.pro@gmail.com>
2023-04-19 13:00:58 +0200

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -5,8 +5,10 @@ import List from "../components/MovieCarousel.jsx";
 const Home = () => {
 
     return (
-        <List params={'/trending/movie/day'}>Tendance</List>
-        <List params='/movie/now_playing'>En ce moment</List>
+        <div>
+            <List params={'/trending/movie/day'}>Tendance</List>
+            <List params={'/movie/now_playing'}>En ce moment</List>
+        </div>
     )
 
 }
