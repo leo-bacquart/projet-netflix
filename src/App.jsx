f0426644f2b945d4db2f2b72641fa5ce52d404a5
Léo <bacquartl@icloud.com>
2023-04-19 09:27:10 +0200

    Call API + Movie Carousel

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,14 @@
 import * as React from 'react';
-import './App.css'
+import './App.scss';
+import List from "./components/MovieCarousel.jsx";
 
 function App() {
-    const [trending, setTrending] = React.useState();
-    React.useEffect(() => {
-        fetch('https://api.themoviedb.org/3/movie/popular?api_key=5c0913b3a2bf3d6729475eaa432c0034&page=1')
-            .then(response => response.json())
-            .then(json => setTrending(json))
-            .catch(error => console.log(error))
-
-        console.log(trending)
-    })
-
-
     return (
         <div>
             <h1>Netflix</h1>
+            <List params='/trending/movie/day'>Tendance</List>
         </div>
     )
 }
-
-
 
 export default App
---
+++ src/components/MovieCarousel.jsx
@@ -0,0 +1,37 @@
+import * as React from "react";
+import * as API from "./callAPI.jsx";
+
+const List = ({params, children}) => {
+    const [movieList, setMovieList] = React.useState([])
+
+    React.useEffect(() => {
+        fetch(`${API.ID}${params}?api_key=${API.KEY}`)
+            .then((response) => response.json())
+            .then((data) => {
+                setMovieList(data.results);
+            })
+            .catch((error) => {
+                console.log(error.message);
+            });
+    }, [])
+
+    return (
+        <div>
+            <h2>{children}</h2>
+            <ul>
+                {movieList.map((movie) => <Item key={movie.key} movie={movie}/> )}
+            </ul>
+        </div>
+    )
+}
+
+
+const Item = ({movie}) => {
+    return (
+        <li>
+            <img src={"https://image.tmdb.org/t/p/w500/" + movie.poster_path} alt={movie.key + "poster"}/>
+        </li>
+    )
+}
+
+export default List
