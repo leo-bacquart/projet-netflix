a26bcfbff65c82ea0ea00790269b51e683f9333f
Ismailvv <131166083+Ismailvv@users.noreply.github.com>
2023-04-26 15:33:08 +0200

    hover(final)

--- src/components/MovieCarousel.jsx
+++ src/components/MovieCarousel.jsx
@@ -12,6 +12,7 @@ import AddIcon from '@mui/icons-material/Add';
 import ThumbUpIcon from '@mui/icons-material/ThumbUp';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import ThumbUpOffAltIcon from '@mui/icons-material/ThumbUpOffAlt';
+import ThumbUpAltIcon from '@mui/icons-material/ThumbUpAlt';
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 const responsive = {
@@ -36,6 +37,8 @@ const responsive = {
 
 const List = ({params, children}) => { /* params = paramètres de recherche dans l'API, children = Prend en paramètre le contenu de la balise */
     const [movieList, setMovieList] = React.useState([]) /* Création state MovieList, définition par un tableau vide */
+    const [transitionPx, setTransitionPx] = React.useState(0)
+    const [isTransition, setIsTransition] = React.useState(false)
     React.useEffect(() => {
         /* Call GET API  */
         fetch(`${API.ID}${params}?api_key=${API.KEY}`) /* Lien d'appel de l'api */
@@ -48,17 +51,51 @@ const List = ({params, children}) => { /* params = paramètres de recherche dans
                 console.log(error.message);
             });
     }, [] /* Pas de dépendance, la fonction s'éxécute une seule fois */)
+
+    const next = () => {
+        setIsTransition(true)
+        if(transitionPx <= -3281) {
+            setTransitionPx(-3281)
+        }else {
+            setTransitionPx(transitionPx - 193)
+        }
+    }
+    const prev = () => {
+        setIsTransition(true)
+        if(transitionPx >= 0) {
+            setTransitionPx(0)
+            setIsTransition(false)
+        }else {
+            setTransitionPx(transitionPx + 193)
+        }
+    }
+    const transitionDisabled = {
+        transition: 'none 0s ease 0s',
+        overflow: 'unset',
+         transform: 'translate3d(0px, 0px, 0px)'
+    }
+    const transitionEnabled = {
+        transition: 'transform 400ms ease-in-out 0s',
+         overflow: 'unset',
+         transform: `translate3d(${transitionPx}px, 0px, 0px)`
+    }
+  
 return (
         <div className={'carousel'}>
             <h2>{children}</h2>
-          
+          {/* transition: transform 400ms ease-in-out 0s; overflow: unset; transform: translate3d(-191px, 0px, 0px); */}
             <div className="react-multi-carousel-list  ul">
                 <ul className="react-multi-carousel-track " 
-                style={{transition: 'none 0s ease 0s', overflow: "'", transform: 'translate3d(0px, 0px, 0px)'}}>
+                style={isTransition === false ?  transitionDisabled : transitionEnabled}>
                 {movieList.map((movie) => <Item key={movie.id} movie={movie}/> )} 
                 </ul>
+<<<<<<< Updated upstream
                     <button aria-label="Go to previous slide" className="react-multiple-carousel__arrow react-multiple-carousel__arrow--left " type="button"></button>
             <button aria-label="Go to next slide" className="react-multiple-carousel__arrow react-multiple-carousel__arrow--right " type="button"></button>
+=======
+                    <button onClick={prev} aria-label="Go to previous slide" class="react-multiple-carousel__arrow react-multiple-carousel__arrow--left " type="button"></button>
+                    <button onClick={next} aria-label="Go to next slide" class="react-multiple-carousel__arrow react-multiple-carousel__arrow--right " type="button"></button>
+>>>>>>> Stashed changes
             </div>
 {/*         
             <Carousel responsive={responsive} className={'ul'}>               
@@ -68,7 +105,16 @@ return (
     )
 }
 
+  const swapIcon = (id) => {
+    console.log(localStorage.getItem(""+id)!=null);
+    localStorage.setItem(""+id, "true"); 
+    
+  };
 
+  const isLiked = (id) =>{
+    console.log(localStorage.getItem(""+id)!=null);
+    return localStorage.getItem(""+id)!=null;
+  };
 
 const Item = ({movie}) => {
     const [estSurvol, setEstSurvol] = React.useState(false);
@@ -82,11 +128,13 @@ const Item = ({movie}) => {
       const handleMouseLeave = () => {
         setEstSurvol(false);
       }
-    //   onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}
+    //onMouseEnter={handleMouseEnter} ;onMouseLeave={handleMouseLeave}
+    console.log(movie);
     return ( 
         <li className={!estSurvol ? 'react-multi-carousel-item react-multi-carousel-item--active li' : 'react-multi-carousel-item react-multi-carousel-item--active li li--box-shadow'} key={movie.key} 
-        style={{flex: '1 1 auto', position: 'relative'}} aria-hidden="false" data-index="0"  onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}
+        style={{flex: '1 1 auto', position: 'relative'}} aria-hidden="false" data-index="0"  
         >
+<<<<<<< Updated upstream
            {
             estSurvol === false 
             ?
@@ -159,48 +207,227 @@ const Item = ({movie}) => {
                          
                         }}>Recommandé à { (movie.vote_average * 10).toFixed(0)}% </p>
                         <p
+=======
+          <div style={{
+        width: "100%",
+        boxShadow: '0px 3px 10px #282727',
+         padding: 0,
+          borderRadius: '8px'
+          
+         }}>
+         <Link to={`details/${movie.id}`} >
+            <img src={"https://image.tmdb.org/t/p/w500/" + movie.poster_path} alt={movie.key + "poster"}
+            style={{
+                width: '100%',
+                objectFit: 'fill',
+                padding: 0
+             }}
+            /> 
+        </Link>
+        <div style={{padding: '1em',    borderRadius: '8px', height: 'auto', position: 'relative',
+top: '-132px'}}>
+                <h3 style={{marginBottom: '8px'}}>{movie.title}</h3>
+            {/*             BUTTON               */}
+            <div style={{display: 'flex', justifyContent: 'space-between'}}>
+                <div>
+                        <button style={{
+                            border: 'none',
+                            cursor: 'pointer'
+                        }}>
+                            <PlayCircleIcon />
+                        </button>
+                        <button
+>>>>>>> Stashed changes
                         style={{
-                            border: '1px solid white',
-                            padding: '2px',
-                            fontSize: '15px',
-                            marginRight: '8px',
-                            marginLeft: '8px'
-                        }}  
-                        >16+</p>
-                        <p>16 Episodes <span 
-                          style={{
-                            border: '1px solid white',
-                            padding: '2px',
-                            borderRadius: '4px',
-                            padding: '0 4px 0 4px'
-                        }} 
-                        >HD</span></p>
-                    </div>
-                    <div style={{display: 'flex', alignItems: 'center'}}>
-                        <p>Sombre</p>
-                        <span style={{
-                            height: '8px',
-                            width: '8px',
-                            backgroundColor: '#646464',
-                            borderRadius: '100px',
-                            margin: '0 4px 0 4px'
-                        }}></span>
-                        <p>Drame</p>
-                        <span style={{
-                            height: '8px',
-                            width: '8px',
-                            backgroundColor: '#646464',
-                            borderRadius: '100px',
-                            margin: '0 4px 0 4px'
-                        }}></span>
-                        <p>Vengeance</p>
-                    </div>
+                            border: 'none',
+                            cursor: 'pointer'
+                        }}
+                        >
+                        <AddCircleOutlineIcon />
+                         
+                        </button>
+                    
+                        
+                        <button onClick={() => swapIcon(movie.id)}
+                        style={{
+                            border: 'none',
+                            cursor: 'pointer'
+                        }}
+                        >
+                            {isLiked(movie.id) ? <ThumbUpOffAltIcon/>: <ThumbUpAltIcon/>}
+                        
+   
+                        </button>
+                </div>
+                <div>
+                            <button
+                            style={{
+                                border: 'none',
+                                cursor: 'pointer'
+                            }}
+                            >
+                            <KeyboardArrowDownIcon />
+                            </button>
                 </div>
             </div>
-         }
-        </li>
-    )
+            <div style={{  margin: '16px 0 16px 0'}}>
+                <p style={{
+                    color: '#46D369',
+                    fontSize: '14px',
+                 
+                }}>Recommandé à { (movie.vote_average * 10).toFixed(0)}% </p>
+                <div style={{display: 'flex', alignItems: 'center', justifyContent: 'space-between', margin: '8px 0 8px 0'}}>
+                <p
+                style={{
+                    border: '1px solid white',
+                    padding: '2px',
+                    fontSize: '13px',
+                    width: '24px'
+                }}  
+                >16+</p>
+                <p>16 Episodes <span 
+                  style={{
+                    border: '1px solid white',
+                    padding: '2px',
+                    borderRadius: '4px',
+                    padding: '0 4px 0 4px'
+                }} 
+                >HD</span></p>
+                </div>
+           
+            </div>
+            <div style={{display: 'flex', alignItems: 'center'}}>
+                <p>Sombre</p>
+                <span style={{
+                    height: '8px',
+                    width: '8px',
+                    backgroundColor: '#646464',
+                    borderRadius: '100px',
+                    margin: '0 4px 0 4px'
+                }}></span>
+                <p>Drame</p>
+                <span style={{
+                    height: '8px',
+                    width: '8px',
+                    backgroundColor: '#646464',
+                    borderRadius: '100px',
+                    margin: '0 4px 0 4px'
+                }}></span>
+                <p>Vengeance</p>
+            </div>
+        </div>
+    </div>
+        </li>}
+estSurvol===false ?
+        <>  
+        <Link to={`details/${movie.id}`}>
+            <img src={"https://image.tmdb.org/t/p/w500/" + movie.poster_path} alt={movie.key + "poster"}/> 
+        </Link>
+    </> 
+    :
+    <div style={{
+        width: "100%",
+      
+         padding: 0,
+          borderRadius: '8px'
+          
+         }}>
+      
+         <Link to={`details/${movie.id}`} >
+            <img src={"https://image.tmdb.org/t/p/w500/" + movie.poster_path} alt={movie.key + "poster"}
+            style={{
+                width: '100%',
+                objectFit: 'fill',
+                padding: 0
+             }}
+            /> 
+        </Link>
+        <div style={{padding: '1em',    borderRadius: '8px', height: 'auto'}}>
+            {/*             BUTTON               */}
+            <div style={{display: 'flex', justifyContent: 'space-between'}}>
+                <div>
+                        <button style={{
+                            border: 'none',
+                            cursor: 'pointer'
+                        }}>
+                            <PlayCircleIcon />
+                        </button>
+                        <button
+                        style={{
+                            border: 'none',
+                            cursor: 'pointer'
+                        }}
+                        >
+                        <AddCircleOutlineIcon />
+                         
+                        </button>
+                        <button
+                        style={{
+                            border: 'none',
+                            cursor: 'pointer'
+                        }}
+                        >
+                        <ThumbUpOffAltIcon />
+                        </button>
+                </div>
+                <div>
+                            <button
+                            style={{
+                                border: 'none',
+                                cursor: 'pointer'
+                            }}
+                            >
+                            <KeyboardArrowDownIcon />
+                            </button>
+                </div>
+            </div>
+            <div style={{display: 'flex', alignItems: 'center', margin: '16px 0 16px 0'}}>
+                <p style={{
+                    color: '#46D369',
+                    fontSize: '16px',
+                 
+                }}>Recommandé à { (movie.vote_average * 10).toFixed(0)}% </p>
+                <p
+                style={{
+                    border: '1px solid white',
+                    padding: '2px',
+                    fontSize: '15px',
+                    marginRight: '8px',
+                    marginLeft: '8px'
+                }}  
+                >16+</p>
+                <p>16 Episodes <span 
+                  style={{
+                    border: '1px solid white',
+                    padding: '2px',
+                    borderRadius: '4px',
+                    padding: '0 4px 0 4px'
+                }} 
+                >HD</span></p>
+            </div>
+            <div style={{display: 'flex', alignItems: 'center'}}>
+                <p>Sombre</p>
+                <span style={{
+                    height: '8px',
+                    width: '8px',
+                    backgroundColor: '#646464',
+                    borderRadius: '100px',
+                    margin: '0 4px 0 4px'
+                }}></span>
+                <p>Drame</p>
+                <span style={{
+                    height: '8px',
+                    width: '8px',
+                    backgroundColor: '#646464',
+                    borderRadius: '100px',
+                    margin: '0 4px 0 4px'
+                }}></span>
+                <p>Vengeance</p>
+            </div>
+        </div>
+    </div>
+ 
+)
 }
-
 export default List
 
